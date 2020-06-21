c8171c6a997b0984dc623254b30961fe2b4d6dbb
Sai Ram Sagar Javvadi <sairamsagar.javvadi@realpage.com>
2020-06-21 13:22:24 +0530

    Initial Commit

---
+++ DatingApp.API/Data/DataContext.cs
@@ -0,0 +1,16 @@
+using DatingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        //Values is the name of the table
+        public DbSet<Value> Values{get;set;}
+    }
+}
